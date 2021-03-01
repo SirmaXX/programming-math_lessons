d56f5e4f9ed425867c1b3facad0c86b29da3228b
Deniz <sea-97@hotmail.com>
2021-03-01 21:03:26 +0300

    Create psudo.c

---
+++ c/psudo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main() {
+ /* 
+ 1.ADIM:BAŞLA
+ 2.ADIM:YAZI YAZDIRALIM
+ 3.ADIM:BİTİR
+
+
+  1.ADIM:BAŞLA
+ 2.ADIM:Kapının yanına git
+ 3.ADIM:KAPI KOLU ÇEVİR
+ 4.ADIM:KAPIDAN ÇIKALIM
+ 5.ADIM BİTİR
+ 
+ uyanıyoruz
+temizlik
+kahvaltı
+okul
+okuldan dönüş
+ders yapımı 
+uyuyalım
+ 
+  printf("kapının yanına git \n");
+  printf("kapı kolu çevir \n");
+   printf("kapıdan çık \n");
+  */
+printf("yazi \n");
+
+}
